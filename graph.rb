692ded78c99699af850d693434f723f7bf11a325
Alperen Bozkurt <mail@alperenbozkurt.net>
2020-04-19 17:39:49 +0300

    Yorum satırları eklendi

--- graph.rb
+++ graph.rb
@@ -2,53 +2,48 @@ load "./node.rb"
 
 class Graph
   attr_accessor :nodes
+
   def initialize(*nodes)
-    if nodes[0].is_a? Array
-      @nodes = []
-      nodes[0].each do |node|
-        @nodes << node
-      end
-    else
-      @nodes = nodes
-    end
+    # Nodes iç içe array olarak gelirse içerdeki arraydekileri ekle
+    @nodes = nodes[0].is_a?(Array) ? nodes[0] : nodes
   end
 
-  def self.create_null_graph(node_count)
+  def self.create_null_graph(node_count)  # n elemanlı boş bir graf oluşturur.
     nodes = []
-    node_count.times do |count|
-      nodes << Node.new(count.to_s)
+    node_count.times do |count|           # n kere döner
+      nodes << Node.new(count.to_s)       # n tane node oluşturur
     end
 
-    return Graph.new(nodes)
+    return Graph.new(nodes.map)           # Oluşturulan node'larla bir graf oluşturup return eder.
   end
 
-  def add_node(node)
+  def add_node(node)        # Grafa ekstra node eklenmek istenirse kullanılır.
     nodes << node
   end
 
-  def add_nodes(nodes)
+  def add_nodes(*nodes)      # Birden fazla node eklenmek istenirse kullanılabilir
     nodes.each do |node|
       @node << node
     end
   end
 
-  def add_edge(node1, node2)
-    node1.add_edge(node2)
+  def add_edge(node1, node2) # iki node birbirine bağlanmak istenirse kullanılır.
+    node1.add_edge(node2)    # bu işlem Node sınıfında halledilir.
   end
 end
 
 
-# g = Graph.create_null_graph(5)
-# puts g.nodes
+# g = Graph.create_null_graph(5)      # 5 nodu olan boş bir graf oluşturur.
+# puts g.nodes                        # Ekrana graftaki bütün nodeları ve komşularını yazdırır.
 
-g2 = Graph.new
+# g2 = Graph.new                      # Yeni bir graf oluşturur
 
-n1 = Node.new("A")
-n2 = Node.new("B")
+# n1 = Node.new("A")                  # Yeni bir node oluşturur
+# n2 = Node.new("B")                  # Yeni bir node oluşturur
 
-g2.add_node(n1)
-g2.add_node(n2)
+# g2.add_node(n1)                     # Grafa node ekler
+# g2.add_node(n2)                     # Grafa node ekler
 
-g2.add_edge(n1, n2)
+# g2.add_edge(n1, n2)                 # Grata iki node arasına bir edge oluşturmak için kullanılır.
 
-puts g2.nodes
+# puts g2.nodes                       # Ekrana graftaki bütün nodeları ve komşularını yazdırır.
--- node.rb
+++ node.rb
@@ -3,7 +3,7 @@ require 'byebug'
 class Node
   attr_accessor :edges, :root
 
-  def initialize(root, *edges)
+  def initialize(root, *edges)    # Node oluşturulurken ismi ve kenarları alınır.
     @root = root
     @edges = edges
     @edges.each do |edge|
@@ -11,14 +11,14 @@ class Node
     end
   end
 
-  def add_edge(node)
+  def add_edge(node)              # kenar yoksa eklenmesi için bu method kullanılır.
     unless @edges.include? node
       @edges << node
-      node.add_edge(self)
+      node.add_edge(self)         # diğer node sınıfını günceller.
     end
   end
 
-  def to_s
+  def to_s      # Düğümü ve komşularını yazdırmak için kullanılan method
     "Bu düğüm : " + self.root + "\n -> Komşuları ise: " + edges.map(&:root).join(', ')
   end
 end
@@ -30,5 +30,5 @@ end
 # c.add_edge(b)             # C düğümü ile a düğümünü bağla
 
 # puts a.to_s
-# puts b.to_s
+# puts b.to_s               # Düğümleri ve komşuları ekrana bastırır.
 # puts c.to_s
